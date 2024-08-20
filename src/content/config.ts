6a82c16178a40e257c6096855a259e9e7586e5c9
Philip Schmitt <philip.schmitt@outlook.com>
2024-08-20 15:55:01 +0200

    Update dependencies

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,18 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	// Type-check frontmatter using a schema
+const blogCollection = defineCollection({
+	type: 'content',
 	schema: z.object({
 		title: z.string(),
 		subheading: z.string(),
 		description: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		tags: z.array(z.string()).optional(),
 	}),
 });
 
-export const collections = { blog };
+export const collections = {
+  'blog': blogCollection,
+};
